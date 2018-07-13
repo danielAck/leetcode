3f0f3af031e06845852aaac5459bb17e63c7c3ec
danieljr <danieljr@126.com>
2018-07-13 21:35:32 +0800

    分治

--- 34_Search for a Range/34_Search for a Range.java
+++ 34_Search for a Range/34_Search for a Range.java
@@ -1,3 +1,8 @@
+/*
+ * 题目地址： https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ * 解题思路： 1. 采用分治的思想，由于数组已经是有序的，直接用中位找不大于target的值快一些，但如果中位是target左右分治获得连续的下标，最后合并左右的下标返回就OK
+ */
+
 class Solution {
     
     public int[] bisearch(int[] list,int l, int r, int target){
@@ -5,9 +10,9 @@ class Solution {
         if(l > r){
             return new int[]{-1,-1};
         }
-        //出现分治后 target 是单个的情况
+        //出现单个的情况
         if(l == r && list[l] == target) return new int[]{l,l};
-        //出现分治后 target 连续的情况
+        //出现全是连续的情况
         if(list[l] == list[r] && list[l] == target) return new int[]{l,r};
         
         int[] res= new int[]{-1,-1};
@@ -18,16 +23,16 @@ class Solution {
         else if (list[mid] > target){
             res = bisearch(list, l, mid-1, target);
         }
-        else{ // 
-            int[] res1 = bisearch(list, l, mid, target);
-            int[] res2 = bisearch(list, mid+1, r, target);
-			// 合并下标, 下标有三种情况：[-1,-1], [2,2], [3,6]
-            if(res1[0] == -1) return res2;
-            else if(res2[0] == -1) return res1;
-            else return new int[]{res1[0],res2[1]};
+        else{   // [-1,-1] - 没找到, [2,2] - 只有一个, [3,6] - 有多个, 下标有三种情况, 合并下标
+            int[] res1 = bisearch(list, l, mid, target); // 得到左边的情况
+            int[] res2 = bisearch(list, mid+1, r, target); // 得到右边的情况
+            if(res1[0] == -1) return res2;                 // 左边没有
+            else if(res2[0] == -1) return res1;            // 右边没有
+            else return new int[]{res1[0],res2[1]};        // 合并
         }
         return res;
     }
+
     public int[] searchRange(int[] nums, int target) {
         return bisearch(nums, 0, nums.length-1, target);
     }
