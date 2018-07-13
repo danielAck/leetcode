7ea44b147dd0d5ef57d2d0486810db4aaeb83060
danieljr <danieljr@126.com>
2018-07-13 20:55:27 +0800

    固定 && 夹逼

--- 18_4Sum/18_4Sum.cpp
+++ 18_4Sum/18_4Sum.cpp
@@ -1,3 +1,8 @@
+/*
+ * 题目地址： https://leetcode-cn.com/problems/4sum/description/
+ * 解题思路： 1. 和 3Sum 一样，只不过多加一层循环，多固定一个数字
+ */
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -5,11 +10,13 @@ public:
         vector<vector<int> > res;
         int n = nums.size();
         if(n < 4)   return res;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){ //多一层判断                                         
             if(i && nums[i] == nums[i-1])     continue;
+
 			//优化，剪枝判断
             if(nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target)    break;	//数组最大和小于target,不存在最优解，直接返回空vector
             if(nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target)    continue;  //当前最大和小于target，剪枝，本轮不可能有解
+
             for(int j = i + 1;j < n;j++) {
                 if(j > i + 1 && nums[j] == nums[j-1])     continue;
                 int lhs = j + 1,rhs = n - 1;
