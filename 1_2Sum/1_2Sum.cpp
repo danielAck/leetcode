edb64903990d5201f589de3aaf4461d8a2654e35
danieljr <danieljr@126.com>
2018-07-13 20:04:07 +0800

    暴力 or Hash

--- 1_2Sum/1_2Sum.cpp
+++ 1_2Sum/1_2Sum.cpp
@@ -1,3 +1,10 @@
+/*
+ * 题目地址： https://leetcode-cn.com/problems/two-sum/description/
+ * 解题思路： 1.暴力两层循环遍历所有情况
+ *          2.使用 HashMap 线性查找 target - i 元素是否存在
+ */
+
+// Solution 1 (C++)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -13,3 +20,19 @@ public:
         }
     }
 };
+
+// Solution 2 (Java)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap <Integer,Integer> map=newHashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int another = target-num;
+            Integer anotherIndex=  map.get(another);
+            if (anotherIndex!=null) {
+                return new int[] {  anotherIndex, i };
+            }
+            map.put(num,i);
+        }
+    }
+}
