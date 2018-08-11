d11147e9e1228c43fb07e57800601957b154484b
danieljr <danieljr@126.com>
2018-08-11 20:51:10 +0800

    数组 && 贪心

--- 122_BestTimeToByAndSellStock2/122_BestTimeToByAndSellStock2.cpp
+++ 122_BestTimeToByAndSellStock2/122_BestTimeToByAndSellStock2.cpp
@@ -3,6 +3,7 @@
  * 解题思路： 1. 价格一升就可以买，一降就可以卖
  */
 
+// my solution
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -29,5 +30,19 @@ public:
         if(!wait)
             profit += (prices[day]-cur);
         return profit;
+    }
+};
+
+// good solution
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0, n = prices.size();
+        for (int i = 0; i < n - 1; ++i) {
+            if (prices[i] < prices[i + 1]) {
+                res += prices[i + 1] - prices[i];
+            }
+        }
+        return res;
     }
 };
